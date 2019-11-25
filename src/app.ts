2f6e27a7509ca580a8febdd765eeaebcec2aea2a
Andre Coelho <andre.ribeiro@iv2.com.br>
2019-11-25 13:15:17 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+class App {
+  public express: express.Application;
+
+  public constructor() {
+    this.express = express();
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+    this.express.use(cors());
+  }
+
+  private database(): void {
+    mongoose.connect('mongodb://localhost:3333/tsnode', {
+      useNewUrlParser: true,
+    });
+  }
+
+  private routes(): void {
+    this.express.get('/', (req, res) => res.send('Hello world'));
+  }
+}
